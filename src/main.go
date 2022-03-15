c80585ea827ade50a621d857798168026a002ea0
Ganesh K <ganeshk.work@gmail.com>
2022-03-15 20:57:08 +0530

    added some activation func

--- src/main.go
+++ src/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println("hello everest")
+}
+
+// ReLU /**
+func ReLU(input float64) float64 {
+	if input < 0 {
+		return 0
+	}
+
+	return input
+}
+
+func LeackyRelU(input float64) float64 {
+	if input < 0 {
+		return input / 10
+	}
+
+	return input
+}
+
+func Sigmoid(input float64) float64 {
+	return 1 / (1 + math.Pow(math.E, input))
 }
